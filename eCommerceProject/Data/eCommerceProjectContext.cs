74157145c2cc5c9dcf512145a4c040ffe4ec2d2e
Marcio Del Bianco <marcio.bianco@gmail.com>
2020-08-09 00:07:23 -0300

    add EFCore, entities props and MySql schema

--- eCommerceProject/Data/eCommerceProjectContext.cs
+++ eCommerceProject/Data/eCommerceProjectContext.cs
@@ -14,6 +14,9 @@ namespace eCommerceProject.Data
         {
         }
 
-        public DbSet<eCommerceProject.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
     }
 }
---
+++ eCommerceProject/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using eCommerceProject.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceProject.Models
+{
+    public class SalesRecord
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SalesStatus SalesStatus { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord() { }
+
+        public SalesRecord(int iD, DateTime date, double amount, SalesStatus salesStatus, Seller seller)
+        {
+            ID = iD;
+            Date = date;
+            Amount = amount;
+            SalesStatus = salesStatus;
+            Seller = seller;
+        }
+    }
+}
